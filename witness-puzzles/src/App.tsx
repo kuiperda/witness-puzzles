6b95b2f5ff48c5ebd5375a6b56595e6de83e1d5d
kuiperda <djkuiper@comcast.net>
2022-09-29 16:25:55 -0400

    basic display working with variable sizing

--- witness-puzzles/src/App.tsx
+++ witness-puzzles/src/App.tsx
@@ -1,9 +1,78 @@
 import './App.css';
-import { PuzzleRender } from './components/PuzzleRender';
+import React, { useState } from 'react';
+import { classicNameResolver, visitEachChild } from 'typescript';
 
 function App() {
+
+  const basic2x2 = [
+    ['v', 'e', 'v', 'e', '-'],
+    ['e', ' ', 'e', ' ', 'e'],
+    ['v', 'e', 'v', 'e', 'v'],
+    ['e', ' ', 'e', ' ', 'e'],
+    ['O', 'e', 'v', 'e', 'v'],
+  ];
+
+  const basic4x4 = [
+    ['v', 'e', 'v', 'e', 'v', 'e', 'v', 'e', '-'],
+    ['e', ' ', 'e', ' ', 'e', ' ', 'e', ' ', 'e'],
+    ['v', 'e', 'v', 'e', 'v', 'e', 'v', 'e', 'v'],
+    ['e', ' ', 'e', ' ', 'e', ' ', 'e', ' ', 'e'],
+    ['v', 'e', 'v', 'e', 'v', 'e', 'v', 'e', 'v'],
+    ['e', ' ', 'e', ' ', 'e', ' ', 'e', ' ', 'e'],
+    ['v', 'e', 'v', 'e', 'v', 'e', 'v', 'e', 'v'],
+    ['e', ' ', 'e', ' ', 'e', ' ', 'e', ' ', 'e'],
+    ['O', 'e', 'v', 'e', 'v', 'e', 'v', 'e', 'v'],
+  ]
+
+  const [grid, setGrid] = useState(basic2x2);
+  const [pageToDisplay, setPageToDisplay] = useState('puzzle');
+  const [puzzle, setPuzzle] = useState(<div>No puzzle here yet</div>);
+
+  const convertGridToDivs = (grid: Array<Array<String>>) => {
+    const puzzleHeight = 80; // sorry, hardcoded, units are vh and it's based on the .puzzle class in the .css file
+    const gridSize = grid.length;
+    const puzzleImage = grid.map((row: Array<String>, yIndex) => {
+      return <div key={`${yIndex}`} className='row' style={{height: `${puzzleHeight / gridSize}vh`}}>
+        {row.map((part: String, xIndex) => {
+        // console.log(`${yIndex}, ${xIndex}`);
+        // return <span key={`${yIndex}.${xIndex}`}>{part}</span>
+        switch(part) {
+          case 'O': {
+            return <span key={`${yIndex}.${xIndex}`} className='entry' style={{width: `${puzzleHeight / gridSize}vh`, height: `${puzzleHeight / gridSize}vh`}}></span>
+          }
+          case 'e': {
+            return yIndex % 2 === 0 ? 
+              <span key={`${yIndex}.${xIndex}`} className='edge' style={{width: `${puzzleHeight / gridSize}vh`, height: `${puzzleHeight / gridSize / 3}vh`}}></span> :
+              <span key={`${yIndex}.${xIndex}`} className='edge' style={{width: `${puzzleHeight / gridSize / 3}vh`, height: `${puzzleHeight / gridSize}vh`}}></span> ;
+          }
+          case 'v': {
+            return <span key={`${yIndex}.${xIndex}`} className='vertex' style={{width: `${puzzleHeight / gridSize}vh`, height: `${puzzleHeight / gridSize}vh`}}></span>
+          }
+          case '-': {
+            return <span key={`${yIndex}.${xIndex}`} className='egress' style={{width: `${puzzleHeight / gridSize}vh`, height: `${puzzleHeight / gridSize}vh`}}></span>
+          }
+          case ' ': {
+            return <span key={`${yIndex}.${xIndex}`} className='empty' style={{width: `${puzzleHeight / gridSize}vh`, height: `${puzzleHeight / gridSize}vh`}}></span>
+          }
+          default: {
+            return <span key={`${yIndex}.${xIndex}`} className='error' style={{width: `${puzzleHeight / gridSize}vh`, height: `${puzzleHeight / gridSize}vh`}}></span>
+          }
+        }
+      })}
+      </div>
+    });
+    return puzzleImage;
+  };
+
+  // TODO: given them variable state while playing
+  const handleClickGenerateButton = () => {
+    setPuzzle(<div>{convertGridToDivs(grid)}</div>);
+    setPageToDisplay('puzzle');
+  }
+
+  const handleClickCustomizeButton = () => setPageToDisplay('customizer');
+
   return (
-    // header with "generate" and "customize" buttons
     // player with puzzle image that lets you play through the puzzle and solve it 
     // eventually, a puzzle solution finder and being able to save puzzles you've solved
     // eventually, a puzzle editor
@@ -11,15 +80,22 @@ function App() {
     <div className='app'>
       <div className='header'>
         <div className='buttons'>
-          <button className='generate'>Generate!</button>
-          <button className='customize'>Customize</button>
+          <button className='generate' onClick={handleClickGenerateButton}>Generate!</button>
+          <button className='customize' onClick={handleClickCustomizeButton}>Customize</button>
         </div>
       </div>
-      <div className='player'>
-        <div className='puzzle'>
-          puzzle goes here
-        </div>
-      </div>
+      <div className='body'>
+        {pageToDisplay === 'puzzle' && 
+          <div className='puzzle'>
+            {puzzle}
+          </div>
+        }
+        {pageToDisplay === 'customizer' && 
+          <div className='customizer'>
+            custom!
+          </div>
+        }
+      </div> 
     </div>
   );
 }
